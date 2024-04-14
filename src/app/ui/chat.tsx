c3ad925dbaa24a62377f908aebfbbd075206bc38
alukan <kolyaterenya@gmail.com>
2024-04-14 21:42:35 +0200

    fixed chat messages and scroll

--- src/app/ui/chat.tsx
+++ src/app/ui/chat.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { PaperAirplaneIcon } from "@heroicons/react/24/outline";
 
 type ChatMessage = {
     sender: string;
@@ -19,10 +20,10 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ messages, sendChatMessage }) =>
         sendChatMessage(message);
         setMessage('');
     };
-    
+
     return (
         <div className="flex flex-col justify-between h-full bg-gray-100 border-l border-gray-300">
-            <div className="p-4 overflow-auto">
+            <div className="p-4 overflow-auto max-h-[65vh]">
                 {messages.map((msg, index) => (
                     <div key={index} className="mb-2 last:mb-0 p-2 bg-white rounded shadow text-black">
                         <strong>{msg.sender}</strong> {msg.date ? <small>{msg.date}</small> : null}
@@ -31,18 +32,22 @@ const ChatWindow: React.FC<ChatWindowProps> = ({ messages, sendChatMessage }) =>
                 ))}
             </div>
             <form onSubmit={handleSubmit} className="flex flex-col p-4 border-t border-gray-300">
-                <input
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    placeholder="Type a message..."
-                    className="mb-4 w-full p-2 text-black border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <button
-                    type="submit"
-                    className="self-end bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-                    Send
-                </button>
+                <div className="relative w-full">
+                    <input
+                        type="text"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        placeholder="Type a message..."
+                        className="pl-4 pr-10 w-full border rounded-full text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <button
+                        type="submit"
+                        className="absolute inset-y-0 right-0 flex items-center pr-3"
+                        aria-label="Send message"
+                    >
+                        <PaperAirplaneIcon className="h-5 w-5 text-blue-500 transform -rotate-45" />
+                    </button>
+                </div>
             </form>
         </div>
     );
