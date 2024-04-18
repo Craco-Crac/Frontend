464cfe9a2de6c10fb9b7ade478737f6a21a200aa
alukan <kolyaterenya@gmail.com>
2024-04-18 20:20:36 +0200

    fixed login form text

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -59,7 +59,7 @@ export default function Login() {
                                     id="username"
                                     type="username"
                                     name="username"
-                                    placeholder="Enter your email address"
+                                    placeholder="Enter your username"
                                     required
                                 />
                                 <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
